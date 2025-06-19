2a30331f77727a6af1d99bb64d08871def115b0f
Armando-Blas <joseblas@seekop.com>
2025-06-18 18:35:23 -0600

    ? commit

--- config/database.js
+++ config/database.js
@@ -2,21 +2,25 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-// Crear la conexión a la base de datos
-const connection = mysql.createConnection({
+// Crear un pool de conexiones para evitar que la conexión se cierre por inactividad
+const connection = mysql.createPool({
     host: process.env.MYSQLHOST,
     port: process.env.MYSQLPORT,
     user: process.env.MYSQLUSER,
     password: process.env.MYSQL_ROOT_PASSWORD,
-    database: process.env.MYSQL_DATABASE
+    database: process.env.MYSQL_DATABASE,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
-// Conexión abierta con éxito
-connection.connect(err => {
+
+connection.getConnection((err, connection) => {
     if (err) {
         console.error('Error al conectar a la base de datos:', err.stack);
         return;
     }
     console.log('Conexión a la base de datos establecida con ID ' + connection.threadId);
+    connection.release();
 });
 
 module.exports = connection;
